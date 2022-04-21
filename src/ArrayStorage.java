fb99889a1b8e01bc79154dfcfce44618fed360aa
A-Nikolaefff <nikron-d@mail.ru>
2022-04-21 17:08:53 +0300

    First homewok has been done

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,74 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                storage[i] = null;
+            } else {
+                return;
+            }
+        }
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                return;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                if (storage[i].uuid == uuid) {
+                    return storage[i];
+                }
+            } else {
+                break;
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        boolean deleted = false;
+        for (int i = 0; i < storage.length; i++) {
+            if (!deleted) {
+                if (storage[i].uuid == uuid) {
+                    storage[i] = null;
+                    deleted = true;
+                }
+            } else {
+                if (storage[i] != null) {
+                    storage[i - 1] = storage[i];
+                } else {
+                    storage[i - 1] = null;
+                    return;
+                }
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int size = size();
+        Resume[] newStorage = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            newStorage[i] = storage[i];
+        }
+        return newStorage;
     }
 
     int size() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                return i;
+            }
+        }
         return 0;
     }
 }
